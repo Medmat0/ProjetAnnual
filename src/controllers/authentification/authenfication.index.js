f5fdbe4e9ce9aaad0ce488de80545435cef97164
Omar EL BARAKA <elbarakaomar99@hotmail.com>
2024-05-17 01:15:55 +0200

    "delete some files

--- src/controllers/authentification/authenfication.index.js
+++
@@ -1,4 +0,0 @@
-export { login } from "./login.js";
-export { register } from "./register.js";
-export { verifyEmail } from "./verifyEmail.js";
-export { logout } from "./logout.js";
--- src/hooks/index.hooks.js
+++
@@ -1,7 +0,0 @@
-import { errorHandler, notFound } from "../middlewares/errorHandler.js";
-import authRouter from "../routes/routes.index.js";
-export default (app) => {
-  app.use("/api/auth", authRouter);
-  app.all("*", notFound);
-  app.use(errorHandler);
-};
