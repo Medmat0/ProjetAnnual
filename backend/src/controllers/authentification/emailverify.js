fcfbd8e7aaae5f78467235ca6176a6a44b5a18f1
Medmat0 <yassineelmatrour@gmail.com>
2024-07-06 14:42:05 +0200

    fix yml backend test

--- backend/src/controllers/authentification/emailverify.js
+++ backend/src/controllers/authentification/emailverify.js
@@ -23,7 +23,6 @@ const verifyEmail = asyncHandler(async (req, res, next) => {
   if (!user) return res.status(400)
   await prisma.user.update({
     where: {
-      
       emailVerificationToken: hashToken,
     },
     data: {
