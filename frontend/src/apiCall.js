69ef9da2ba5c92ed133fa6e6cffe0f4b992af5a9
Medmat0 <yassineelmatrour@gmail.com>
2024-07-21 20:29:55 +0200

    fix creation post bug final one v1 front

--- frontend/src/apiCall.js
+++ frontend/src/apiCall.js
@@ -1,3 +1,3 @@
 export const BASE_URL_DOCKER = "https://179f-20-0-152-200.ngrok-free.app";
-//export const BASE_URL = "https://socialcode-backend.azurewebsites.net";  
-export const BASE_URL = "http://localhost:3000";  
+export const BASE_URL = "https://socialcode-backend.azurewebsites.net";  
+  
