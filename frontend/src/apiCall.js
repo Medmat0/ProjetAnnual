664f18c51b576fdc28f71dca6756c79eec13937f
Medmat0 <yassineelmatrour@gmail.com>
2024-07-22 14:59:24 +0200

    fix url backend docker

--- frontend/src/apiCall.js
+++ frontend/src/apiCall.js
@@ -1,3 +1,3 @@
-export const BASE_URL_DOCKER = "https://179f-20-0-152-200.ngrok-free.app";
+export const BASE_URL_DOCKER = "https://a8fb-20-0-152-200.ngrok-free.app";
 export const BASE_URL = "https://socialcode-backend.azurewebsites.net";  
   
