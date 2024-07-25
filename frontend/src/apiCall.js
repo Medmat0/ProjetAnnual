44742491bbf92c038fbc3e8a0e5f230ff402648b
Medmat0 <yassineelmatrour@gmail.com>
2024-07-25 11:19:03 +0200

    add link

--- frontend/src/apiCall.js
+++ frontend/src/apiCall.js
@@ -1,3 +1,3 @@
-export const BASE_URL_DOCKER = "https://1f43c34f193e285ed5714d052a70f80e.serveo.net";
+export const BASE_URL_DOCKER = " https://ea722d306d5502f138f72682667fb935.serveo.net";
 export const BASE_URL = "https://socialcode-backend.azurewebsites.net";  
   
